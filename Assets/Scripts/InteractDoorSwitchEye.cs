39acce65eb349bc206251b9a790e633ba72aecd8
Ryan Ngoon <ryan.ngoon@gmail.com>
2022-05-07 14:10:33 -0700

    Add creaking door

--- Assets/Scripts/InteractDoorSwitchEye.cs
+++ Assets/Scripts/InteractDoorSwitchEye.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class InteractDoorSwitchEye : Interactable
 {
-    [SerializeField] Animator door = null; 
+    [SerializeField] Animator door = null;
+    [SerializeField] AudioClip ac;
 
-    void Update()
-    {
-        
-    }
 
     public override void interact()
     {
         door.SetBool("isOpen", true);
+        GetComponent<AudioSource>().PlayOneShot(ac, .9f);
     }
 }
